918facb39af0774984c5231af4abd54aadea41bf
2065205 <2065205@CL245-10>
2017-03-14 11:59:03 +0900

    復習開始

--- VB（T_T）/Module1.vb
+++ VB（T_T）/Module1.vb
@@ -1,7 +1,19 @@
 ﻿Module Module1
 
     Sub Main()
-
+        Dim a, b
+        a = 1
+        b = "2"
+        Trace.WriteLine(a + b)
     End Sub
+
+    ' Private Function Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs)
+    'Dim a, b
+    '   a = 1
+    '  b = "2"
+    '& Trace.WriteLine(a + b)
+    'End Function
+
+
 
 End Module
